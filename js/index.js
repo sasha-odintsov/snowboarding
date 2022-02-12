784117c36f0cef3fee0a72387ef7dece3907d823
Sasha <aleks-one@ukr.net>
2022-02-12 15:48:26 +0200

    Added character movement

--- js/index.js
+++ js/index.js
@@ -7,16 +7,21 @@ const field = [
   [0, 0, 0, 0, 0, 0]
 ];
 
-function render() { 
+let x = 0;
+let y = 0;
+
+function render(area) { 
   htmlMarkup = '';
-  field.forEach((row) => {
+  area.forEach((row, indexRow) => {
     htmlMarkup = htmlMarkup + '<div class="row">'
-    row.forEach((item) => {
+    row.forEach((item, indexColumn) => {
       htmlMarkup = htmlMarkup + '<div class="column">'
       if (item === 1) {
-        htmlMarkup = htmlMarkup + '<img class="trees" src="images/trees.png">';
+        htmlMarkup = htmlMarkup + '<img class="trees" src="images/trees.png">'; 
       } else if (item === 3) {
         htmlMarkup = htmlMarkup + '<img class="rider" src="images/rider.png">';
+        x = indexRow;
+        y = indexColumn;
       }
       htmlMarkup = htmlMarkup + '</div>'
     });
@@ -24,45 +29,84 @@ function render() {
   });
   return htmlMarkup;
 };
-document.getElementById('area').innerHTML = render();
+document.getElementById('area').innerHTML = render(field);
+
+console.log(x);
+console.log(y);
+
+document.addEventListener('keyup', (event) => {
+  if (event.key === 'ArrowRight') {
+    const countColumns = field[0].length;
+    if (y < countColumns - 1) {      
+      field[x][y] = 0;
+      field[x][y + 1] = 3;
+      document.getElementById('area').innerHTML = render(field); 
+      console.log(x);
+      console.log(y);
+    }
+  }
+  if (event.key === 'ArrowDown') {
+    const countRows = field.length;
+    if (x < countRows - 1) {
+      field[x][y] = 0;
+      field[x + 1][y] = 3;
+      document.getElementById('area').innerHTML = render(field);
+      console.log(x);
+      console.log(y);
+    }
+  }
+  if (event.key === 'ArrowUp') {
+    const countRows = field.length;
+    if (x < countRows - 1) {
+      field[x][y] = 0;
+      field[x - 1][y] = 3;
+      document.getElementById('area').innerHTML = render(field);
+      console.log(x);
+      console.log(y);
+    }
+  }
+  if (event.key === 'ArrowLeft') {
+    const countColumns = field[0].length;
+    if (y < countColumns - 1) {
+      field[x][y] = 0;
+      field[x][y - 1] = 3;
+      document.getElementById('area').innerHTML = render(field); 
+      console.log(x);
+      console.log(y);
+    }
+  }
+});
 
 
-
-// let addRow = document.createElement('div');
-// addRow.className = "field";
-// addRow.innerHTML = "Hi!";
-
-// let rider = document.createElement('img');
-// rider.className = "rider";
-// rider.setAttribute('src', 'images/rider.png');
-// document.getElementById('area').appendChild(rider);
-
-// let trees = document.createElement('img');
-// trees.className = "trees";
-// trees.setAttribute('src', 'images/trees.png');
-// document.getElementById('area').appendChild(trees);
-
-// function render() { 
-//   field.forEach((row, indexRow) => {
-//     let addRow = document.createElement('div');
-//     addRow.className = "field";
-//     document.getElementById('area').append(addRow);    
-//     row.forEach((item, indexCol) => { 
-//       if (elem === 1) { 
-//         let trees = document.createElement('img');
-//         trees.className = "trees";
-//         trees.setAttribute('src', 'images/trees.png');
-//         document.getElementById('area').appendChild(trees);
-//       }
-//     })
-//     elem.forEach((elem) => { 
-//       if (elem === 3) {
-//         let rider = document.createElement('img');
-//         rider.className = "rider";
-//         rider.setAttribute('src', 'images/rider.png');
-//         document.getElementById('area').appendChild(rider);
-//       }
-//     })  
-//   });  
-// }
-// render();
+document.querySelector('.controls__right').addEventListener(('click'), () => {
+  const countColumns = field[0].length;
+    if (y < countColumns - 1) {
+      field[x][y] = 0;
+      field[x][y + 1] = 3;
+      document.getElementById('area').innerHTML = render(field);
+    }
+});
+document.querySelector('.controls__left').addEventListener(('click'), () => {
+  const countColumns = field[0].length;
+    if (y < countColumns - 1) {
+      field[x][y] = 0;
+      field[x][y - 1] = 3;
+      document.getElementById('area').innerHTML = render(field); 
+    }
+});
+document.querySelector('.controls__up').addEventListener(('click'), () => {
+  const countRows = field.length;
+    if (x < countRows - 1) {
+      field[x][y] = 0;
+      field[x - 1][y] = 3;
+      document.getElementById('area').innerHTML = render(field);
+    }
+});
+document.querySelector('.controls__down').addEventListener(('click'), () => {
+  const countRows = field.length;
+    if (x < countRows - 1) {
+      field[x][y] = 0;
+      field[x + 1][y] = 3;
+      document.getElementById('area').innerHTML = render(field);
+    }
+});
